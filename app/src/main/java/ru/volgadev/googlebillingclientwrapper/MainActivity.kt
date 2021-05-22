7533318de4acba159f61c2decc045f727cbf9dff
Maxim <maxim@ubuntu.ubuntu-domain>
2021-05-22 23:31:33 +0300

    add lib to app dependency

--- app/src/main/java/ru/volgadev/googlebillingclientwrapper/MainActivity.kt
+++ app/src/main/java/ru/volgadev/googlebillingclientwrapper/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.volgadev.googlebillingclientwrapper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.volgadev.googlebillingclientwrapper.api.PaymentManagerFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val paymentManager = PaymentManagerFactory.createPaymentManager(this)
     }
 }
